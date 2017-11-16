16ef09400e183b4b6ce5cf12de3cc5d365f0cd1c
Jasmine Beeman <jasmine.beeman@metalab.csun.edu>
2017-11-15 17:24:38 -0800

    figure layout out

--- resources/views/layouts/templates/template.blade.php
+++ resources/views/layouts/templates/template.blade.php
@@ -23,71 +23,85 @@
   </div> {{--container --}}
 </div> {{--section --}}
 															{{-- BUTTONS --}}
-<div class="section">
-  <div class="nav-container">  
+<div class="section__button">
+  <div class="container__button">  
     <a href="#" target="_blank" class="button__right type--link type--red"><i class="fa fa-long-arrow-left margin-right" aria-hidden="true"></i>Button</a>
     <a href="#" target="_blank" class="pull-right button__left type--link type--red" id="type--plus">Button<i class="fa fa-long-arrow-right margin-left" aria-hidden="true"></i></a>
   </div>
 </div>
 															{{-- END BUTTONS --}}
-{{-- <div class="section">
-	<div class="container">
-		
-		<div class="col-md-6 container__full--left">
-      <div class="blue-bar">
-        <div class="img">
-            <img class="img__side-image" src="https://pbs.twimg.com/media/DC3Xke3XUAMX7iv.jpg" alt="Side Image">
-        </div>
-      </div>
-		</div>
-			
 
-			<div class="col-md-6">
-				<div class="parallax"></div>
+<div class="section__knight">
+  <div class="container__knight">
 
-					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque hic, eligendi veniam, laudantium, eius a molestias in quis numquam id et animi, temporibus aspernatur rerum. Sunt, reiciendis libero minima!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id porro iusto, numquam quod neque, necessitatibus laboriosam cupiditate mollitia unde voluptatum! Rerum laboriosam nemo possimus atque, necessitatibus, sit earum quaerat soluta.</p>
-
-			</div>
-
-	</div>
-</div> --}}
-
-<div class="section__meta">
-  
-
-  <div class="container__meta">
     <div class="meta-md-6">
-      <div class="blue-bar">
-        <div class="img">
-          <img class="img__side-image" src="https://pbs.twimg.com/media/DNOFvxkUMAAV_5x.jpg" alt="Side Image">
+      <div class="container__img--right">
+        <div class="blue-box">
+          <div class="img__main">
+            <img class="img__side" src="https://pbs.twimg.com/media/DNOFvxkUMAAV_5x.jpg" alt="Side Image">
+          </div>
         </div>
       </div>
     </div>
 
-    <div class="col-md-6">
-      <div class="parallax">
-        <img src="../public/img/D.svg" class="parallax__img" alt="">
-      </div>
+    <div class="meta-md-6">
+      <div class="container__content">
+        <div class="parallax">
+          <img src="../public/img/D.svg" class="parallax__img" alt="">
+        </div>
 
-      <div class="intro">
-        <h4>HELLO WORLD</h4>
-        <div class="title__bar"></div>
+        <div class="intro">
+          <h4>HELLO WORLD</h4>
+          <div class="title__bar"></div>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
 
+        </div>
       </div>
     </div>
+
   </div>
-
-
-
-
 </div>
 
+<div class="section__knight">
+  <div class="container__knight">
+
+    <div class="meta-md-6">
+      <div class="container__content">
+        <div class="parallax">
+          <img src="../public/img/F.svg" class="parallax__img" alt="">
+        </div>
+
+        <div class="intro">
+          <h4>HELLO WORLD</h4>
+          <div class="title__bar"></div>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="meta-md-6">
+      <div class="container__img--left">
+        <div class="blue-box pull-right">
+          <div class="img__main">
+            <img class="img__side pull-right" src="https://pbs.twimg.com/media/DNOFvxkUMAAV_5x.jpg" alt="Side Image">
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
 
 
 
