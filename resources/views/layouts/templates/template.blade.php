d6f24d4c7efa64cc146b19ac977aceb714292eef
Jasmine Beeman <jasmine.beeman@metalab.csun.edu>
2017-11-15 17:51:48 -0800

    need to work on media quires

--- resources/views/layouts/templates/template.blade.php
+++ resources/views/layouts/templates/template.blade.php
@@ -1,26 +1,22 @@
 @extends('layouts.master')
 @section('title', 'Write Page Name Here') 
 @section('structure')
-<div class="section">
+<div class="section__banner">
   <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        													{{-- BANNER --}}
-        <div class="banner__img" style="background-image: url(https://pbs.twimg.com/media/DC3Xke3XUAMX7iv.jpg);">
-          <div class="container pre-overlay">
-            <div class="row">
-              <div class="col-md-12">
-                <div class="banner__flex--cover">
-                   <h1 class="banner__flex--title type--white">BANNER NAME</h1>
-                </div>
-              </div>
-            </div>
+  <div class="col-md-12">
+    													{{-- BANNER --}}
+    <div class="banner__img" style="background-image: url(https://pbs.twimg.com/media/DC3Xke3XUAMX7iv.jpg);">
+      <div class="container pre-overlay">
+        <div class="col-md-12">
+          <div class="banner__flex--cover">
+             <h1 class="banner__flex--title type--white">BANNER NAME</h1>
           </div>
-          <div class="black-overlay"></div>
         </div>
-      </div> {{--col-md-12 --}}
-    </div> {{--row --}}
-  </div> {{--container --}}
+      </div>
+      <div class="black-overlay"></div>
+    </div>
+  </div> {{--col-md-12 --}}
+ </div> {{--container --}}
 </div> {{--section --}}
 															{{-- BUTTONS --}}
 <div class="section__button">
@@ -38,7 +34,7 @@
       <div class="container__img--right">
         <div class="blue-box">
           <div class="img__main">
-            <img class="img__side" src="https://pbs.twimg.com/media/DNOFvxkUMAAV_5x.jpg" alt="Side Image">
+            <img class="img__side" src="http://78.media.tumblr.com/26f1db79797aaf8577a32e288c5afefd/tumblr_n7yhhvUQtx1st5lhmo1_1280.jpg" alt="Side Image">
           </div>
         </div>
       </div>
@@ -94,8 +90,44 @@
       <div class="container__img--left">
         <div class="blue-box pull-right">
           <div class="img__main">
-            <img class="img__side pull-right" src="https://pbs.twimg.com/media/DNOFvxkUMAAV_5x.jpg" alt="Side Image">
+            <img class="img__side pull-right" src="http://torontoparadise.com/wp-content/uploads/2015/06/unsplash.jpg" alt="Side Image">
           </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="section__knight">
+  <div class="container__knight">
+
+    <div class="meta-md-6">
+      <div class="container__img--right">
+        <div class="blue-box">
+          <div class="img__main">
+            <img class="img__side" src="https://cdn.betakit.com/wp-content/uploads/2017/05/unsplash-hq-03.jpg" alt="Side Image">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="meta-md-6">
+      <div class="container__content">
+        <div class="parallax">
+          <img src="../public/img/D.svg" class="parallax__img" alt="">
+        </div>
+
+        <div class="intro">
+          <h4>HELLO WORLD</h4>
+          <div class="title__bar"></div>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat asperiores ullam dolor iusto, ipsam ad incidunt aliquam placeat necessitatibus! Blanditiis laboriosam dicta sint nobis. Eveniet corrupti amet voluptas porro quos?</p>
+
         </div>
       </div>
     </div>
